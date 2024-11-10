468c7cc7e415aab5d7aeb5121c3d5be45ccb7f40
sming0305 <sming0305@gmail.com>
2024-11-10 08:17:17 +0800

    nuxt-hotel-booking-init

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const route = useRoute();
+
+console.log(route)
+</script>
+
+<template>
+  <div>
+    Page: index
+    <p>目前路由的路徑 - route.fullpath 為 : {{ route.fullPath }}</p>
+  </div>
+</template>
+
+<style scoped></style>
