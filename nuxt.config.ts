8eb4bdf8ba6d65c471f299a2275b11eec37cb262
sming0305 <sming0305@gmail.com>
2024-11-13 21:44:04 +0800

    nuxt.config 設置 全域配置 & 全域變數

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,16 +5,24 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   // 啟用 Nuxt DevTools 開發工具
   devtools: { enabled: true },
+  // 假設 all.scss 文件中引入了自訂的 Bootstrap 配置、字體和基礎樣式，這些設定會在應用啟動後自動生效，您不需要再手動將 all.scss 引入到每個頁面或元件。
   css: ["@/assets/stylesheets/all.scss"],
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
+          // 在 Nuxt.config.ts 設定全域共用 bootstrap5 的 SCSS 變數 ，並且可以直接使用在 .vue 檔案內的 <style></style>
+          // 使用了 vite.css.preprocessorOptions.scss.additionalData 設置，這可以讓所有 SCSS 文件（如元件內的樣式）在編譯前自動包含 _variables.scss。
           additionalData: `
-            @import "@/assets/stylesheets/_variables.scss"; // 在全部的元件都引入 _variables.scss
-          `,
+          @import "bootstrap/scss/functions"; 
+          @import "bootstrap/scss/variables";
+          @import "@/assets/stylesheets/_variables.scss"; `, // 在全部的元件都引入 _variables.scss
         },
       },
     },
   },
 });
+
+
+// css 屬性將 all.scss 全域加載。
+// additionalData 避免在每個 SCSS 文件中重複引入變數。
--- pages/index.vue
+++ pages/index.vue
@@ -4,16 +4,32 @@ const route = useRoute();
 
 <template>
   <div>
+    <!-- Day 1 - 起手式 : 建立 Nuxt3 專案  -->
     <h1 class="title">Hello world</h1>
     Page: index
     <p>目前路由的路徑 - route.fullpath 為 : {{ route.fullPath }}</p>
+
+
+    <!-- Day 2 - Nuxt3 專案引入 CSS 樣式 -->
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+
+    <button type="button" class="btn btn-link">Link</button>
   </div>
 </template>
 
 <style lang="scss">
 $primary: blue;
 
+// 在 Nuxt.config.ts 設定全域共用 bootstrap5 的 SCSS 變數，這邊就可以直接使用  vite.css.preprocessorOptions.scss 內引入的 @import "bootstrap/scss/functions"; 
+// 以及 @import "bootstrap/scss/variables"; 內的變數了
 h1 {
-  color: $primary
+  color: $indigo
 }
 </style>
