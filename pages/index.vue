a9d314e71f5c80a0e83d1872546e6ad4fa9a2f7a
sming0305 <sming0305@gmail.com>
2024-11-13 21:20:12 +0800

    套用全域 css

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
 const route = useRoute();
-
-console.log(route)
 </script>
 
 <template>
   <div>
+    <h1 class="title">Hello world</h1>
     Page: index
     <p>目前路由的路徑 - route.fullpath 為 : {{ route.fullPath }}</p>
   </div>
 </template>
 
-<style scoped></style>
+<style lang="scss">
+$primary: blue;
+
+h1 {
+  color: $primary
+}
+</style>
